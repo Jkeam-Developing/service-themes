174fc2c7d606ca636653d07b1d54c8e1882d353e
Jkeam-Developing <157502414+Jkeam-Developing@users.noreply.github.com>
2025-03-13 14:57:10 +0100

    Update custom_text_header.js

--- css/addons/unraid/login-page/retro-terminal/js/custom_text_header.js
+++ css/addons/unraid/login-page/retro-terminal/js/custom_text_header.js
@@ -6,11 +6,12 @@
 // YOU MUST ESCAPE ANY SINGLE BACKSLASHES LIKE SO: \\
 
 let custom_text_header = `
-<div class="custom-text-header"> <pre>   __  __                 _     __   __  __           __ 
+<div class="custom-text-header"> <pre>
+   __  __                 _     __   __  __           __ 
   / / / /___  _________ _(_)___/ /  / / / /___  _____/ /_
- / / / / __ \/ ___/ __ `/ / __  /  / /_/ / __ \/ ___/ __/
+ / / / / __ \\/ ___/ __ `/ / __  /  / /_/ / __ \\/ ___/ __/
 / /_/ / / / / /  / /_/ / / /_/ /  / __  / /_/ (__  ) /_  
-\____/_/ /_/_/   \__,_/_/\__,_/  /_/ /_/\____/____/\__/  
+\\____/_/ /_/_/   \__,_/_/\\__,_/  /_/ /_/\\____/____/\\__/  
                                                          /pre> </div>
 `;
 document.getElementById("login").innerHTML += custom_text_header
